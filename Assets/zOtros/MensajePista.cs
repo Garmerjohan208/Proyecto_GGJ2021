0206e0f7bf0ea5d5ee0017ef0e7456e4cf7fb4bf
HernandoNJ <hernandonj.unity@gmail.com>
2021-01-30 18:29:22 -0500

    Se agrego carpeta z otros

---
+++ Assets/zOtros/MensajePista.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MensajePista : MonoBehaviour
+{
+    [SerializeField] private GameObject textoMensaje;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            textoMensaje.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            textoMensaje.SetActive(false);
+        }
+    }
+}
---
+++ Assets/zOtros/PersonajeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PersonajeController : MonoBehaviour
+{
+    private float speed = 20f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movePlayer = new Vector3(moveHorizontal, 0, moveVertical);
+        transform.Translate(movePlayer * speed * Time.deltaTime);
+    }
+
+}
