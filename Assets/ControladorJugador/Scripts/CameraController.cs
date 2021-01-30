78720633b6e7f9cf1dd60dbb035cb708ad88aba4
Garmerjohan208 <78266498+Garmerjohan208@users.noreply.github.com>
2021-01-30 17:52:44 -0500

    Actualizando Archivos

---
+++ Assets/ControladorJugador/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float RotationSpeed = 1;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+
+        mouseY = Mathf.Clamp(mouseY, -15, 35);
+
+        transform.LookAt(Target);
+
+
+        if (Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("a") || Input.GetKey("d"))
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+        else 
+        {
+
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+
+        }
+         
+
+    }
+}
---
+++ Assets/ControladorJugador/Scripts/PlayerMovementController.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{   
+    //Variables de control de velocidad
+    [SerializeField] float CurrentSpeed = 5, WalkingSpeed = 5, SideAndBackSpeed = 1.5f;
+
+    //Inicializa varibles del movimiento
+    float moveHorizontal, moveVertical;
+    Vector3 moveCube;
+
+    //Inicializa variables del animador
+    Animator animator;
+    int isRunningHash, isGoingBackHash,isGoingSideA,isGoingSideD;
+    bool walkingUp, isWalkingUp, walkingBack, isWalkingBack, walkingToTheSideA, isWalkingToTheSideA, walkingToTheSideD, isWalkingToTheSideD;
+
+    private void Start()
+    {
+        //Recursos del animador
+        animator = GetComponent<Animator>();
+        isRunningHash = Animator.StringToHash("isWalking");
+        isGoingBackHash = Animator.StringToHash("WalkBack");
+        isGoingSideA = Animator.StringToHash("isGoingSideA");
+        isGoingSideD = Animator.StringToHash("isGoingSideD");
+    }
+
+    private void Update()
+    {
+        //Movimiento
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        moveCube = new Vector3(moveHorizontal, 0f, moveVertical);
+        transform.Translate(moveCube * CurrentSpeed * Time.deltaTime);
+
+        //Animate Going Up
+        walkingUp = Input.GetKey("w");
+        isWalkingUp = animator.GetBool(isRunningHash);
+
+        if (walkingUp && !isWalkingUp) 
+        {
+
+            animator.SetBool(isRunningHash, true);
+        
+        }
+
+        if (!walkingUp && isWalkingUp)
+        {
+
+            animator.SetBool(isRunningHash, false);
+
+        }
+
+        //Animate Going Back
+        walkingBack = Input.GetKey("s");
+        isWalkingBack = animator.GetBool(isGoingBackHash);
+
+        if (walkingBack && !isWalkingBack)
+        {
+
+            animator.SetBool(isGoingBackHash, true);
+
+        }
+
+        if (!walkingBack && isWalkingBack)
+        {
+
+            animator.SetBool(isGoingBackHash, false);
+
+        }
+
+        //Animate Walking To The Side and Speed Control:
+        //D
+        walkingToTheSideD = Input.GetKey("d");
+        isWalkingToTheSideD = animator.GetBool(isGoingSideD);
+
+        if (walkingToTheSideD && !isWalkingToTheSideD)
+        {
+
+            animator.SetBool(isGoingSideD, true);
+            if (!walkingUp && !walkingBack)
+            CurrentSpeed = SideAndBackSpeed;
+
+        }
+
+        if (!walkingToTheSideD && isWalkingToTheSideD)
+        {
+
+            animator.SetBool(isGoingSideD, false);
+            CurrentSpeed = WalkingSpeed;
+        }
+
+        //A
+        walkingToTheSideA = Input.GetKey("a");
+        isWalkingToTheSideA = animator.GetBool(isGoingSideA);
+
+        if (walkingToTheSideA && !isWalkingToTheSideA)
+        {
+
+            animator.SetBool(isGoingSideA, true);
+            if (!walkingUp && !walkingBack)
+            CurrentSpeed = SideAndBackSpeed;
+
+        }
+
+        if (!walkingToTheSideA && isWalkingToTheSideA)
+        {
+
+            animator.SetBool(isGoingSideA, false);
+            CurrentSpeed = WalkingSpeed;
+
+        }
+
+    }
+}
