76576bd47952d34f93c78aa80bbe29e269fc3029
HernandoNJ <hernandonj.unity@gmail.com>
2021-01-30 23:26:25 -0500

    Carpeta zOtros removida, creacion de escenas
    Los archivos se trasladaron a PREFABESCENA
    Creacion de prefabs de controladores de escena (no tocar)
    Creacion de escenas StartScene y FinalScene

---
+++ Assets/PREFABESCENA/Scripts/GameSceneController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameSceneController : MonoBehaviour
+{
+    [SerializeField] private AudioSource environmentAudio;
+    [SerializeField] private AudioSource churchAudio;
+    [SerializeField] private AudioSource forestAudio;
+    [SerializeField] private AudioSource waterpillarAudio;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
