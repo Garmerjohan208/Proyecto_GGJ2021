34331fff5f2c22934ec880aa9e62df92965d9040
HernandoNJ <hernandonj.unity@gmail.com>
2021-01-31 11:49:37 -0500

    Added Particle Systems

---
+++ Assets/PREFABESCENA/Scripts/LoadGameScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameScene : MonoBehaviour
+{
+    public void LoadEscenaPaisaje()
+    {
+        SceneManager.LoadScene("Escena paisaje");
+    }
+}
